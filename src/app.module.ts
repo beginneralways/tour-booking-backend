43348568d220bfeabb13fb6ef0da06a328f55913
Prashant Chaudhary <donem7510@gmail.com>
2024-02-09 12:20:21 +0545

    Created_necessary_modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ToursModule } from './tours/tours.module';
+import { BookingsModule } from './bookings/bookings.module';
 
+// eslint-disable-next-line prettier/prettier
 @Module({
-  imports: [],
+  imports: [AuthModule, ToursModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
